3bc53b04dc2f0cc3a195ff43e8bcc86069d5ac1a
Lizenyang <120714723+Lizenyang@users.noreply.github.com>
2025-01-02 17:11:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ bot_token="7830106860:AAF_tDStMZZugfcrl3zWrdARswHMTVLCCok"        # 你的 Teleg
 chat_id="5553145286"            # 你的 Telegram 用户 ID
 
 # 发送设备ID和IPv4地址到 Telegram
-message="IP+4449: $ipv4_address
+message="IP+4449: $ipv4_address:4449
 设备ID是: $device_id 
 EarnApp 注册链接：$https_link"
 # 对消息进行 URL 编码
