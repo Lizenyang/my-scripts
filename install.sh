f2934503f3eb060fb644c2e8e54f54606c08e2e9
Lizenyang <120714723+Lizenyang@users.noreply.github.com>
2025-01-06 16:33:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ color_echo() {
 color_echo "${GREEN}" "🎉🎉🎉恭喜老板喜提新机🎉🎉🎉"
 #------------------------------------------------------------------------------------------------------------
 # 更新ALL
+# curl 安装
+apt update -y  && apt install -y curl
 # 删除旧的 Docker 仓库配置
 sudo rm /etc/apt/sources.list.d/docker.list
 # 添加 Debian Bullseye 的 Docker 仓库
